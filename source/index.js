d7cd4387152bfb7bfaf1f57e3c6822d92b0da7aa
Sergio-Ka <33621369+Sergio-Ka@users.noreply.github.com>
2018-09-25 00:27:35 +0700

    fix

--- source/index.js
+++ source/index.js
@@ -1,9 +1,5 @@
-import './index.styl';
-import './blocks/common/header-of-block/images/icon-profile.png';
-import './blocks/common/header-of-block/images/icon-contact.png';
-import './blocks/common/header-of-block/images/icon-skills.png';
-import './blocks/common/header-of-block/images/icon-education.png';
-import './blocks/common/header-of-block/images/icon-experience.png';
-import './blocks/common/header-of-block/images/icon-software.png';
-import './blocks/common/content-list__item/images/list-item-dot.png';
-import './images/favicon.ico';
+const requireAllFiles = function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+};
+
+requireAllFiles(require.context('./', true, /^\.\/.*\.(styl|png)$/));
