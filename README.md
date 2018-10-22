20cd4f7d1f0d79b642b4643e398d5c7a0e932f6b
Sergio-Ka <33621369+Sergio-Ka@users.noreply.github.com>
2018-10-23 02:17:51 +0700

    #7 fix readme

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # MySummary
 My Summary
 
-GitHab Pages: https://sergio-ka.github.io/MySummary/
+Сверстанное резюме. Учебный проект. Разбито на элементы в соответствии с методологией БЭМ. Стартовый файл запуска в статике index.html из папки /public.
 
-Сверстанное резюме. Учебный проект. Разбито на элементы в соответсвии с методологией БЭМ. Стартовый файл запуска в статике index.html из папки /public.
+GitHab Pages:
+https://sergio-ka.github.io/MySummary/
+
+Клонирование репозитория:
+git clone https://github.com/Sergio-Ka/MySummary.git
+
+Установка модулей для сборки:
+npm i (или npm install)
 
 Команды:
-- разовая сборка: npm run build,
-- сервер с live reload: npm run server.
+- разовая сборка для production: npm run build,
+- сборка для разработки (с опцией -watch и без минификации js-файла сборки): npm run dev,
+- сервер с live reload: npm run server, http://localhost:8080/.
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './source/index.js' /*{
-    pug: './index.pug'
-  }*/,
+  entry: './source/index.js',
 
   output: {
     path: __dirname + '/public',
@@ -16,29 +16,29 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin('style.css'),
-    new HtmlWebpackPlugin({template: 'source/index.pug', filename: './index.html'})
+    new HtmlWebpackPlugin({template: 'source/index.pug', filename: './index.html'}),
   ],
 
   module: {
 
     loaders: 
     [
-    {
-      test: /\.pug$/,
-      loader: 'pug-loader'
-    },
-    {
-      test: /\.styl$/,
-      loader: ExtractTextPlugin.extract('css-loader!stylus-loader')
-    },
-    {
-      test: /\.(svg|ttf|otf|eot|woff)$/,
-      loader: 'file-loader?name=fonts/[name].[ext]'
-    },
-    {
-      test: /\.(png|ico)$/,
-      loader: 'file-loader?name=images/[name].[ext]'
-    }
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('css-loader!stylus-loader')
+      },
+      {
+        test: /\.(svg|ttf|otf|eot|woff)$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(png|ico)$/,
+        loader: 'file-loader?name=images/[name].[ext]'
+      }
     ]
 
   },
@@ -50,3 +50,14 @@ module.exports = {
   }
 
 };
+
+  if (NODE_ENV == 'production') {
+    module.exports.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: true,
+          drop_console: true,
+          unsafe: true,
+        },
+    }));
+  };
+
