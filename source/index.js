cd83ae46afe906fe0579bae1d283355b0c5de15d
Sergio84 <kin-gall@mail.ru>
2020-01-23 13:11:50 +0700

    make-gh-pages

--- source/index.js
+++
@@ -1,5 +0,0 @@
-const requireAllFiles = function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-};
-
-requireAllFiles(require.context('./', true, /^\.\/.*\.(styl|png)$/));
